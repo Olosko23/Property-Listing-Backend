c6b495347266b154b24864604a7e6e55837b1b0d
Olosko23 <oloogeorge633@gmail.com>
2023-10-18 13:48:02 +0300

    'protect'

--- routes/propertyRoutes.js
+++ routes/propertyRoutes.js
@@ -15,14 +15,14 @@ import { isLandlord } from "../middleware/isLandlord.js";
 
 const router = express.Router();
 
-router.post("/", createProperty);
-router.get("/:propertyId", getPropertyById);
-router.put("/:propertyId", updateProperty);
-router.get("/", getAllProperties);
-router.get("/rent", getPropertiesForRent);
-router.get("/sale", getPropertiesForSale);
-router.get("/search", getPropertiesByLocationAndPrice);
-router.get("/type/:type", getPropertiesByType);
-router.get("/purpose/:purpose", getPropertiesByPurpose);
+router.post("/", protect, isLandlord, createProperty);
+router.get("/:propertyId", protect, getPropertyById);
+router.put("/:propertyId", protect, isLandlord, updateProperty);
+router.get("/", protect, getAllProperties);
+router.get("/rent", protect, getPropertiesForRent);
+router.get("/sale", protect, getPropertiesForSale);
+router.get("/search", protect, getPropertiesByLocationAndPrice);
+router.get("/type/:type", protect, getPropertiesByType);
+router.get("/purpose/:purpose", protect, getPropertiesByPurpose);
 
 export default router;
